184c98988c56e6fba1a98882a9219e4d9a86b0b9
Riafi <s6rifink@uni-bonn.de>
2025-01-28 21:25:54 +0100

    geändert:       calc_radii.py

--- calc_radii.py
+++ calc_radii.py
@@ -34,8 +34,28 @@ for i in range(0,len(bmaj)):
     item_maj.append(bmaj)
     item_min.append(bmin)
     item_r.append(r_hl)
+
+beam_345 = pd.read_csv('data_band_both_100GHz_dooff.csv')
+item_maj_345 = []
+item_min_345 = []
+item_r_345 = []
+bmaj_345 = beam_345[' bmaj_value'].to_numpy()
+bmin_345 = beam_345[' bmin_value'].to_numpy()
+flux_345 = beam_345[' flux_value'].to_numpy()
+flux_345 = 10**3*flux_345
+flux_err_345 =beam_345[' flux_error'].to_numpy()
+flux_err_345 = 10**3*flux_err_345
+    #calculte major and minor axis in pc
+bmaj_345 = np.deg2rad(bmaj_345/3600) * dis
+bmin_345 = np.deg2rad(bmin_345/3600) * dis
+    # calculate geometric mean of bmaj and bmin
+r_hl_345 = np.sqrt(bmaj_345*bmin_345)
+for i in range(0,len(bmaj_345)):
+    item_maj.append(bmaj_345)
+    item_min.append(bmin_345)
+    item_r.append(r_hl_345)
 f = f'{flux} $\pm$ {flux_err}'
-dict = {'S_{100GHz}' : flux ,'b_{maj} ': bmaj, '$b_{min}$ ': bmin, '$R_{hl}$ ': r_hl}
+dict = {'$S_{{100GHz}}$' : flux , '$S_{{345GHz}}$' : flux_345, '$b_{{maj,100GHz}}$ ': bmaj, '$b_{{min,100GHz}}$ ': bmin, '$R_{{hl,100GHz}}$ ': r_hl }
 df = pd.DataFrame(dict)
    
 print(df) 
