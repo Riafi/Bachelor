e7a04bffd7497d42b3f9346d67faeec599806d1e
Riafi <s6rifink@uni-bonn.de>
2025-02-06 12:04:58 +0100

    geändert:       S_free_dust_contributions.pdf 	geändert:       calc_flux.py 	geändert:       calc_radii.py 	geändert:       totalmasstohalflightradius.pdf

--- calc_radii.py
+++ calc_radii.py
@@ -15,29 +15,29 @@ dis = 44*10**6 # Distance to ngc3256
 
 
 
-beam = pd.read_csv('data_both_deconvolved_radii.csv')
+beam2 = pd.read_csv('data_both_deconvolved_radii.csv')
 item_maj = []
 item_min = []
 item_r = []
-bmaj = beam[' bmaj'].to_numpy()
-bmaj_err = beam[' bmaj_error'].to_numpy()
-bmin = beam['bmin'].to_numpy()
-bmin_err = beam['bmin_error'].to_numpy()
+bmaj2 = beam2[' bmaj'].to_numpy()
+bmaj_err2 = beam2[' bmaj_error'].to_numpy()
+bmin2 = beam2['bmin'].to_numpy()
+bmin_err2 = beam2['bmin_error'].to_numpy()
 
     #calculte major and minor axis in pc
-bmaj = np.deg2rad(bmaj/3600) * dis
-bmaj_err = np.deg2rad(bmaj_err/3600) * dis
-bmin = np.deg2rad(bmin/3600) * dis
-bmin_err = np.deg2rad(bmin_err/3600) * dis
+bmaj2 = np.deg2rad(bmaj2/3600) * dis
+bmaj_err2 = np.deg2rad(bmaj_err2/3600) * dis
+bmin2 = np.deg2rad(bmin2/3600) * dis
+bmin_err2 = np.deg2rad(bmin_err2/3600) * dis
     # calculate geometric mean of bmaj and bmin
-r_hl = np.sqrt(bmaj*bmin)
-r_hl_err = np.sqrt((bmin_err**2*bmaj**2 + bmaj_err**2 * bmin**2)/(bmin*bmaj))
-for i in range(0,len(bmaj)):
-    item_maj.append(bmaj)
-    item_min.append(bmin)
-    item_r.append(r_hl)
-decon = beam['deconvolved_not deconvolved']
-dict = { '$b_{{maj}}$ ': bmaj, '$b_{{min}}$ ': bmin, '$R_{{hl}}$ ': r_hl, '$\Delta R_{hl}$' : r_hl_err,  'Region deconvolved' : decon }
+r_hl2 = np.sqrt(bmaj2*bmin2)
+r_hl_err2 = np.sqrt((bmin_err2**2*bmaj2**2 + bmaj_err2**2 * bmin2**2)/(bmin2*bmaj2))
+for i in range(0,len(bmaj2)):
+    item_maj.append(bmaj2)
+    item_min.append(bmin2)
+    item_r.append(r_hl2)
+decon = beam2['deconvolved_not deconvolved']
+dict = { '$b_{{maj}}$ ': bmaj2, '$b_{{min}}$ ': bmin2, '$R_{{hl}}$ ': r_hl2, '$\Delta R_{hl}$' : r_hl_err2,  'Region deconvolved' : decon }
 df = pd.DataFrame(dict)
    
 print(df) 
