76ab74d13bca1b8a72e78e37a50461f19efa2405
Riafi <s6rifink@uni-bonn.de>
2025-02-06 09:40:35 +0100

    geändert:       S_free_dust_contributions.pdf 	geändert:       calc_flux.py 	neue Datei:     totalmasstohalflightradius.pdf

--- calc_flux.py
+++ calc_flux.py
@@ -6,6 +6,9 @@ from astropy.utils.data import get_pkg_data_filename
 import matplotlib.colors as clr
 import pandas as pd
 
+light = 299792458
+k_b=1.380649*10**(-23)
+dis=44*10**6
 
 # read in the values from the data and convert the flux values as well as the flux error values from Jy to mJy
 #beam_values reads in the data from the 100GHz sourceband
@@ -88,8 +91,28 @@ print ('percentage of free free emission at 100GHz', (S_ff100/flux)*100)
 radi = pd.read_csv('data_bandboth_100GHz_deconvolved.csv')
 radi3 = pd.read_csv('data_bandboth_345GHz_deconvolved.csv')
 #read in the major and minor axis
-maj_345 = radi3[' bmaj'].to_numpy()
-min_345 = radi3['bmin'].to_numpy()
+a = radi[' bmaj'].to_numpy()
+a =a[0:10]
+b = radi3[' bmaj'].to_numpy()
+b=b[10:23]
+bmaj = np.append(a,b)
+print (bmaj)
+c = radi['bmin'].to_numpy()
+c=c[0:10]
+d = radi3['bmin'].to_numpy()
+d=d[10:23]
+bmin = np.append(c,d)
+ae = radi[' bmaj_error'].to_numpy()
+ae =ae[0:10]
+be = radi3[' bmaj_error'].to_numpy()
+be=be[10:23]
+bmaj_err = np.append(ae,be)
+print (bmaj)
+ce = radi['bmin_error'].to_numpy()
+ce=ce[0:10]
+de = radi3['bmin_error'].to_numpy()
+de=de[10:23]
+bmin_err = np.append(ce,de)
 #read in the peak intensity of the regions and their error
 peak_345 = beam_345['peak_value'].to_numpy()
 peak_345 = 10**3*peak_345 # converting Jy/beam into mJy/beam
@@ -98,23 +121,31 @@ peak_e_345 = 10**3*peak_e_345 #converting Jy/beam into mJy/beam
 #print(peak_345, '\pm', peak_e_345)
 
 #calculate the peak brightness temperature using formula  from website (https://science.nrao.edu/facilities/vla/proposing/TBconv) 
-T_b = 1.222*10**3*(peak_345/(345**2*maj_345*min_345))
+T_b = 1.222*10**3*(peak_345/(345**2*bmaj*bmin))
+T_b_err = np.sqrt((1222*peak_e_345/(345**2*bmaj*bmin))**2+((1222*peak_345*bmin_err)/(345**2*bmaj*bmin**2))**2 + ((1222*peak_345*bmaj_err)/(345**2*bmin*bmaj**2))**2)
 print('brightness Temperature of peak intensity at 345GHz is',T_b)
 #T_B = ((8.69*10**(-4))**2)*S_dust/(2*np.pi*1.381*((np.deg2rad(maj_345/3600)*np.deg2rad(min_345/3600))/4*np.log(2)))
 #print ('brightness Temperature,' , T_B)
-T= (flux345*10**(-3))*(13.6*(300/345)**2*(1/min_345)*(1/maj_345))
-print('Temperatur',T)
 
+
+T= (flux345*10**(-3))*(13.6*(300/345)**2*(1/bmin)*(1/bmaj))
+T_err = np.sqrt((flux345_err*10**(-3)*(13.6*(300/345)**2*(1/bmin)*(1/bmaj)))**2 + (flux345*10**(-3)*13.6*(300/345)**2*(bmin_err/(bmin**2))*(1/bmaj))**2 +(flux345*10**(-3)*13.6*(300/345)**2*(bmaj_err/(bmaj**2))*(1/bmin))**2)
+print('Temperatur',T)
+T_sd = flux345*10**(-3)*0.63*12**2/3514
+print ('TemperaturSingledish', T_sd)
 #calculate gas temperature
 T_gas = 11.07/(np.log(1+ 11.07/(T+0.195)))
+T_gas_err = 122.545*T_err/((T+0.195)*(T+11.265)*np.log(11.07/(T+0.195)+1)**2)
 print('gas Temperature at 345GHz is', T_gas)
 
 #calculate dust mass using equation 6 from paper
 M_dust= 74.220*S_dust*(44**2)*((np.exp(17/T_gas)-1)/0.9)
+M_dust_err =np.sqrt((74.220*eS_dust*(44**2)*((np.exp(17/T_gas)-1)/0.9))**2 + (74.220/0.9*S_dust*(44**2)*(np.exp(17/T_gas))*17/(T_gas**2)*T_gas_err)**2)
 print ('dust Mass is', np.log10(M_dust))
 
 #calculating gas mass using equation 7 from paper
 M_gas = 120*M_dust
+M_gas_err = 120*M_dust_err
 print('gas Mass is', np.log10(M_gas))
 
 #calculate luminosity of the free free emission using S_ff and distance of the ngc3256 at 44 Mpc
@@ -123,16 +154,45 @@ print ('luminosity ', L_vt)
 
 #calculate ionizing photon rates using equation 3 from paper sun et al.
 Q_0s = 1.6*10**52 * S_ff100 * ((44/10)**2) *((6000/6000)**(-0.51))
+Q_0s_err = 1.6*10**52 * err_S_ff100 *(44/10)**2 *((6000/6000)**(-0.51))
 print ('Q_0s is', Q_0s)
 M_stars = Q_0s / (4.0*10**46)
+Mstars_err = Q_0s_err/(4.0*10**46)
 
 print ('stellar mass calculated is', np.log10(M_stars))
 #calculate ionizing photon rates using equation 9 from paper He et al.
 Q_0h = 6.3 * (10**25) * ((1000/1000)**(-0.45))*(100**0.1)*(L_vt)
 M_starh = Q_0h/(4.0*10**46) 
 print ('ionizing photon number and Stellar mass', Q_0h, np.log(M_starh))
+
+#total mass
+M_tot = M_gas + M_stars
+M_tot_err = np.sqrt(M_gas_err**2 + Mstars_err**2)
+
+
+#calculate radii
+bmaj_rad = np.deg2rad(bmaj/3600) * dis
+bmaj_rad_err = np.deg2rad(bmaj_err/3600) * dis
+bmin_rad = np.deg2rad(bmin/3600) * dis
+bmin_rad_err = np.deg2rad(bmin_err/3600) * dis
+r_hl = np.sqrt(bmaj_rad*bmin_rad)
+r_hl_err = np.sqrt((bmin_rad_err**2*bmaj_rad**2 + bmaj_rad_err**2 * bmin_rad**2)/(bmin_rad*bmaj_rad))
+m=np.linspace(10**6,10**9,1000)
+r_beam  = 0*m+ 19.198621772
+fig,ax = plt.subplots()
+ax.margins(0)
+plt.errorbar(M_tot,r_hl, yerr = r_hl_err/(r_hl *np.log(10)), xerr= np.abs(M_tot_err/(M_tot *np.log(10))),  color='midnightblue', marker='+',capsize=2,  linestyle='none', zorder = 3.5)
+plt.plot(m,r_beam, color='lightgray',linestyle='--', zorder=2.5)
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('$M_{tot}$ [$M_{\odot}$]')
+plt.ylabel('$R_{hl}$ [pc]')
+ax.set_xlim(left = 10**6 , right =10**9, auto = True)
+ax.set_ylim(bottom = 10**(0) , top = 10**2 ,auto =True)
+plt.savefig('totalmasstohalflightradius.pdf')
+plt.show()
  
-dict = {'$S_{{100GHz}}$' : flux , '$S_{100GHz,ff}$' : S_ff100, '$S_{{345GHz}}$' : flux345, '$S_{345GHz,dust}$' : S_dust , '$M_{Gas}$' : np.log10(M_gas) , '$M_{stellar}$' : np.log10(M_stars) }
+dict = {'$S_{{100GHz}}$' : flux , '$S_{100GHz,ff}$' : S_ff100, '$S_{{345GHz}}$' : flux345, '$S_{345GHz,dust}$' : S_dust , '$M_{Gas}$' : np.log10(M_gas) , '$\Delta M_{gas}$': M_gas_err/(M_gas *np.log(10)), '$M_{stellar}$' : np.log10(M_stars),'$R_{{hl}}$ ': r_hl, '$\Delta R_{{hl}}$ ': r_hl_err }
 df = pd.DataFrame(dict)
    
 print(df) 
